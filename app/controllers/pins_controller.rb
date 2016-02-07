45dba1f73465cbfe4b24cc1b4136e171503e510a
Manmeet Saluja <manmeet.saluja@u.nus.edu>
2016-02-08 02:42:00 +0800

    refactored and beautified ruby code

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+    @pins = Pin.all.order('created_at DESC').paginate(page: params[:page], per_page: 12)
   end
 
   def show
@@ -40,18 +40,19 @@ class PinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
 
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description, :image)
-    end
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: 'Not authorized to edit this pin' if @pin.nil?
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def pin_params
+    params.require(:pin).permit(:description, :image)
+  end
 end
